efd69cd02371b307baf0bf2802e13572abd7daaf
andrewskylark <andrew.skylark@gmail.com>
2021-09-10 20:08:25 +0400

    homework-3

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,13 +1,19 @@
 import s from './style.module.css';
 
-const Layout = ({title, descr, urlBg, colorBg}) => {
-    const layoutStyle = {
-        backgroundImage: urlBg && `url("${urlBg}")`,
-        backgroundColor: colorBg
-    };
+const Layout = ({title, id, urlBg, colorBg, children}) => {
+    const layoutStyle = {};
+
+    if (urlBg) {
+        layoutStyle.backgroundImage = `url("${urlBg}")`;
+    }
+    if (colorBg) {
+        layoutStyle.backgroundColor =  colorBg;
+    }
+
     return (
         <section 
         className={s.root}
+        id={id}
         style= {layoutStyle}
         >
             <div className={s.wrapper}>
@@ -17,7 +23,7 @@ const Layout = ({title, descr, urlBg, colorBg}) => {
                         <span className={s.separator}></span>
                     </div>
                     <div className={`${s.desc} ${s.full}`}>
-                        <p>{ descr }</p>
+                        {children}
                     </div>
                 </article>
             </div>
