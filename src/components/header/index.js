10bc6f8b574189549a1267c2f5ef4674f50a6362
andrewskylark <andrew.skylark@gmail.com>
2021-09-16 08:28:36 +0400

    Task#2 adds header styles

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,6 +5,8 @@ const Header = ({title, descr, onClickBtn}) => {
     return (
         <header className={s.root}> 
             <div className={s.forest}></div>
+            <div className={s.siluette}></div>
+            <div className={s.moon}></div>
             <div className={s.container}>
                 <h1>{ title }</h1>
                 <p>{ descr }</p>
