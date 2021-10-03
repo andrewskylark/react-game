2dcc3fbd320336a24445cc994efc9ae9cbf12385
andrewskylark <andrew.skylark@gmail.com>
2021-10-03 13:22:09 +0400

    refactoring of loginForm

--- src/components/menuHeader/index.js
+++ src/components/menuHeader/index.js
@@ -9,6 +9,9 @@ import LoginForm from '../loginForm';
 const MenuHeader = ({ bgActive }) => {
     const [isOpened, setOpened] = useState(false);
     const [isOpenedModal, setOpenedModal] = useState(false);
+    const SIGN_UP_POST = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB5F8VIr6hcBgKoQAN6yWJ-c145EiJmZdA';
+    const SIGN_IN_POST = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB5F8VIr6hcBgKoQAN6yWJ-c145EiJmZdA';
+    
     const onClickMenu = () => {
         setOpened(!isOpened);
     }
@@ -18,8 +21,7 @@ const MenuHeader = ({ bgActive }) => {
     const onClickLogin = () => {
         setOpenedModal(prevState => !prevState);
     }
-    const onSubmitLoginForm = async ({email, password, auth}) => {
-        // console.log(values)
+    const onSubmitLoginForm = async ({email, password, register}) => {
         const requestOptions = {
             method: 'POST',
             body: JSON.stringify({
@@ -28,20 +30,25 @@ const MenuHeader = ({ bgActive }) => {
                 returnSecureToken: true,
             })
         }
-        let responce;
-        if (auth === false) {
-            responce = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB5F8VIr6hcBgKoQAN6yWJ-c145EiJmZdA', requestOptions).then(res => res.json());
-        }
-        if (auth === true) {
-            responce = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB5F8VIr6hcBgKoQAN6yWJ-c145EiJmZdA', requestOptions).then(res => res.json());
-        }
+
+        const responce = await fetch(
+            register ? SIGN_UP_POST : SIGN_IN_POST, requestOptions)
+            .then((res) => res.json()
+        );
+ 
         if (responce.hasOwnProperty('error')) {
             NotificationManager.error(responce.error.message, 'Error!');
+            alert('error')
         } else {
-            if (auth === true) {
-                localStorage.setItem('idToken', responce.idToken)
+            alert('success')
+            if (register === false) {
+                localStorage.setItem('idToken', responce.idToken);
+                alert('setTocekn')
+                onClickLogin();
             }
-            NotificationManager.success('Success!');
+            NotificationManager.success(
+                register ? 'Registered succesfully!' : 'Signed in, welcome back!'
+            );
         }
         
     }
@@ -63,6 +70,7 @@ const MenuHeader = ({ bgActive }) => {
                 onCloseModal={onClickLogin}
             >
                 <LoginForm
+                    isResetField={!isOpenedModal}
                     onSubmit={onSubmitLoginForm}
                 />
             </Modal>
